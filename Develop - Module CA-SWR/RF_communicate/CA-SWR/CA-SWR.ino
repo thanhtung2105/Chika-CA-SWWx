6790128aa79424bd027fa4baefe6058007902215
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2020-02-26 17:36:39 +0700

    Update CA-SWR.ino

--- Develop - Module CA-SWR/RF_communicate/CA-SWR/CA-SWR.ino
+++ Develop - Module CA-SWR/RF_communicate/CA-SWR/CA-SWR.ino
@@ -1,5 +1,13 @@
+#include <EEPROM.h>
 #include <RF24.h>
 #include <SPI.h>
+
+/*
+	Product code: 2a0a6b88-769e-4a63-ac5d-1392a7199e88
+	RF channel (1 button): 83878226022001
+	Button topic: be47fa93-15df-44b6-bdba-c821a117cd41
+*/
+
 
 /* In this product - the address (channel) to communicate is define as SWR (convert to decimal) 
 + 'date make device' + 'product no.' ; In this case, SWR is known as '83 87 82' and add with the date 
@@ -7,31 +15,30 @@ making device for example today is Feb 17th and this is the first product in tha
 for this SWR is: const byte address[15] = "83878217022001"	( 83 87 82 | 17 02 20 | 01 )					*/
 
 RF24 radio(9, 10);   //nRF24L01 (CE,CSN) connections PIN
-const uint40_t address[15] = "83878217022001";
+const byte address[15] = "83878226022001";    //Changeable
 
-const int button_1 = 6;
-const int button_2 = 7;
+const int button = 5;
+const int control = 2;
 
-const int control_1 = 4;
-const int control_2 = 5;
+int deviceState;
+int buttonState;             
+int lastButtonState = LOW;   
 
-boolean stateDEVICE_control_1 = false;
-boolean stateDEVICE_control_2 = false;
+long lastDebounceTime = 0;  
+long debounceDelay = 50;    
 
-boolean state_Button_send[2];
-boolean state_Button_control[2];
+boolean state_Device_sendtoHC[1];
+boolean state_Device_controlfromHC[1];
 
 void setup() {
   SPI.begin();
   Serial.begin(9600);
   Serial.println("\nCA-SWR say hello to your home <3 ! ");
-  pinMode(button_1, INPUT);
-  pinMode(button_2, INPUT);
-  pinMode(control_1, OUTPUT);
-  pinMode(control_2, OUTPUT);
-  digitalWrite(control_1, LOW);
-  digitalWrite(control_2, LOW);
+  pinMode(button, INPUT);
+  pinMode(control, OUTPUT);
   
+  digitalWrite(control, deviceState);
+  checkDeviceState();
   radio.begin();
   radio.setRetries(15, 15);
   radio.setPALevel(RF24_PA_MAX);
@@ -42,53 +49,45 @@ void loop()
   radio.openReadingPipe(1, address);
   radio.startListening();
   
-    if (radio.available())
+  if (radio.available())
   {
-   memset(&state_Button_control, ' ', sizeof(state_Button_control));
-	 radio.read(&state_Button_control, sizeof(state_Button_control));
-   digitalWrite(control_1, state_Button_control[0]);
-   stateDEVICE_control_1 = state_Button_control[0];
-   digitalWrite(control_2, state_Button_control[1]);
-   stateDEVICE_control_2 = state_Button_control[1];
+   memset(&state_Device_controlfromHC, ' ', sizeof(state_Device_controlfromHC));
+	 radio.read(&state_Device_controlfromHC, sizeof(state_Device_controlfromHC));
+   digitalWrite(control, state_Device_controlfromHC[0]);
+   deviceState = state_Device_controlfromHC[0];
   }
   
-  boolean check_Button_1 = isButton_Click(button_1);
-  boolean check_Button_2 = isButton_Click(button_2);
+  int check_Button = digitalRead(button);
   
-  if (check_Button_1)
-	{
-		radio.stopListening();
-		stateDEVICE_control_1 = !stateDEVICE_control_1;
-		digitalWrite(control_1, stateDEVICE_control_1);		
-		state_Button_send[0] = stateDEVICE_control_1;
-		state_Button_send[1] = stateDEVICE_control_2;
-		
-		radio.openWritingPipe(address);
-		radio.write(&state_Button_send, sizeof(state_Button_send));
-	}
-	
-	if (check_Button_2)
-	{
-    radio.stopListening();
-		stateDEVICE_control_2 = !stateDEVICE_control_2;
-		digitalWrite(control_2, stateDEVICE_control_2);		
-		state_Button_send[0] = stateDEVICE_control_1;
-		state_Button_send[1] = stateDEVICE_control_2;
-		
-		radio.openWritingPipe(address);
-		radio.write(&state_Button_send, sizeof(state_Button_send));
-	}
- 
-  delay(100);
+  if(check_Button != lastButtonState) {
+	  lastDebounceTime = millis();
+  }
+  if((millis() - lastDebounceTime) > debounceDelay) {
+	  if (check_Button != buttonState) {
+		  buttonState = check_Button;
+		  if (buttonState = HIGH) {
+			  deviceState = !deviceState;
+			  }
+	  }
+  }
+  
+  radio.stopListening();
+  digitalWrite(control, deviceState);
+  state_Device_sendtoHC[0] = deviceState;
+  EEPROM.update(0, deviceState);
+  
+  radio.openWritingPipe(address);
+  radio.write(&state_Device_sendtoHC[0], sizeof(state_Device_sendtoHC));
+  
+  lastButtonState = check_Button;
 }
 
-int isButton_Click(int GPIO_to_read)
-{
-    int out = 0;
-    while (digitalRead(GPIO_to_read) == 1)
-    {
-        delay(20);
-        out = 1;
-    }
-    return out;
+void checkDeviceState() {
+   deviceState = EEPROM.read(0);
+   if(deviceState == 1) {
+    digitalWrite(control, HIGH);
+   } 
+   if(deviceState == 0) {
+    digitalWrite(control, LOW);
+   }
 }
